e4e6415e25181ad689df1fd57625a5bb5f3b5255
dangdinhsi <siddd00474@fpt.edu.vn>
2019-06-15 10:46:26 +0700

    updload git

---
+++ src/main/java/model/MemberModel.java
@@ -0,0 +1,32 @@
+package model;
+
+
+import entity.Member;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MemberModel {
+public Member checkLogin(String username,String password){
+
+    try {
+        String sql = "select * from member where username = ? and password = ?";
+        PreparedStatement prep = DBHelper.getConnection().prepareStatement(sql);
+        prep.setString(1,username);
+        prep.setString(2,password);
+        ResultSet rs = prep.executeQuery();
+        if(rs.next()){
+            String rsUsername = rs.getString("username");
+            String rsPassword = rs.getString("password");
+            Member member = new Member();
+            member.setUsername(rsUsername);
+            member.setPassword(rsPassword);
+            return member;
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return null;
+}
+}
---
+++ web/login.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 6/15/2019
+  Time: 10:22 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/login" method="post">
+    Username: <input type="text" name="username">
+    <br>
+    Password: <input type="text" name="password">
+    <input type="submit" value="LOGIN">
+</form>
+</body>
+</html>
